c31bebfe1e94d0e69db5bebc19c29e86ca4f4e06
DevolTime <yefriathertua@gmail.com>
2026-03-25 20:12:53 -0500

    ejercicios

---
+++ script.js
@@ -0,0 +1,23 @@
+// let nombre = prompt("cual es tu nombre?");
+// let edad = Number (prompt("cual es tu edad?"));
+// let email = prompt("cual es tu correo?");
+// console.log(nombre, edad, email);
+
+//Descuento
+// let precio = Number (prompt("precio del articulo"));
+// let descuento = Number (prompt("escoje tu descuento"));
+// let precioDescuento = (precio * descuento)/100;
+// alert("precio del articulo: " + precioDescuento);
+// let total = (precio - precioDescuento);1
+// alert("precio total: " + total);
+
+//kilometros a metros
+// let km = Number(prompt("Metros"));
+// let m = (m / 1000);
+// alert(km + " kilometros");
+
+//metros a kilometros
+// let km = Number(prompt("kilometros"));
+// let m = (km * 1000);
+// alert(m + " Metros");
+
